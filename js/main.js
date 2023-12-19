68b86916b12b7636f6e093282ba8ae6d9086f280
Ikhal <ismuhammadmi7@gmail.com>
2023-12-19 01:32:03 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const responsive ={
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    }
+}
+$(document).ready(function(){
+    $nav = $('nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** CLick event on toggle menu  */
+
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('.collapse')
+    })
+
+    //owl carousel for the blog
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:false,
+        autoplayTimeout:3000,
+        dots:false,
+        nav: true, 
+        navText:[$('.owl-nav .owl-nav-prev'), $('.owl-nav .owl-nav-next')],
+        responsive:responsive
+    });
+
+    //click to scroll up
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop:0
+        }, 1000);
+    })
+ })
